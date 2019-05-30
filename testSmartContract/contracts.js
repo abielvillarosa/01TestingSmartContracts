4fea31907341ef23f596cc4db37da1aaffeecd0b
Abiel Villarosa <john_abiel@yahoo.com>
2019-05-29 22:02:47 -0400

    Embark Lab

--- testSmartContract/contracts.js
+++ testSmartContract/contracts.js
@@ -4,7 +4,7 @@ module.exports = {
     // Blockchain node to deploy the contracts
     deployment: {
       host: "localhost", // Host of the blockchain node
-      port: 8546, // Port of the blockchain node
+      port: 8555, // Port of the blockchain node
       type: "ws" // Type of connection (ws or rpc),
       // Accounts to use instead of the default account to populate your wallet.
       // The order here corresponds to the order of `web3.eth.getAccounts`, so the first one is the `defaultAccount`
@@ -53,9 +53,9 @@ module.exports = {
 
     contracts: {
       // example:
-      //SimpleStorage: {
-      //  args: [ 100 ]
-      //}
+      Raffle: {
+       args: [ 100 ]
+      }
     }
   },
 
--- testSmartContract/test/contract_spec.js
+++ testSmartContract/test/contract_spec.js
@@ -1,29 +1,48 @@
 // /*global contract, config, it, assert*/
-/*
-const SimpleStorage = require('Embark/contracts/SimpleStorage');
+
+const Raffle = require('Embark/contracts/Raffle');
 
 let accounts;
 
 // For documentation please see https://embark.status.im/docs/contracts_testing.html
 config({
-  //deployment: {
-  //  accounts: [
-  //    // you can configure custom accounts with a custom balance
-  //    // see https://embark.status.im/docs/contracts_testing.html#Configuring-accounts
-  //  ]
-  //},
   contracts: {
-    "SimpleStorage": {
+    "Raffle": {
       args: [100]
     }
+    // "Raffle": {
+    //   args: accounts
+    // }
   }
 }, (_err, web3_accounts) => {
   accounts = web3_accounts
 });
 
-contract("SimpleStorage", function () {
+contract("Raffle", function () {
   this.timeout(0);
 
+  it("Raffle was deployed", async function() {
+    //Raffle has an address
+    let address = Raffle.options.address;
+    assert.ok(address); //has a value, and not null
+  });
+
+  it("suspendRaffle is working", async function() {
+
+    try {
+      let result = await Raffle.methods.suspendRaffle().call(); 
+    }
+    catch {
+      assert.ok("Caller is not the owner");
+    }
+  })
+
+  // it("SetInteger and getInteger are tested", async function() {
+  //   await SimpleStorage.methods.setInteger(5).send();
+  //   let result = await SimpleStorage.methods.getInteger().call();
+  //   assert.strictEqual(parseInt(result, 10), 5);
+  // });
+/** 
   it("should set constructor value", async function () {
     let result = await SimpleStorage.methods.storedData().call();
     assert.strictEqual(parseInt(result, 10), 100);
@@ -38,6 +57,6 @@ contract("SimpleStorage", function () {
   it("should have account with balance", async function() {
     let balance = await web3.eth.getBalance(accounts[0]);
     assert.ok(parseInt(balance, 10) > 0);
-  });
-}
-*/
+  }); 
+  */
+})
